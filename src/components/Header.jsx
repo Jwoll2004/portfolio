a1b7518aa731631edfd23897c6a623f54628a7dd
Prajwal Tiwari <fnafchica2004@gmail.com>
2024-06-25 21:57:55 +0530

    Minor files created

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+// header.css present in styles directory
+import React from 'react';
+import '../styles/Header.css';
+
+function Header() {
+	  return (
+	<header>
+	  <h1>Portfolio Manager</h1>
+	</header>
+  );
+}
+
+export default Header;
